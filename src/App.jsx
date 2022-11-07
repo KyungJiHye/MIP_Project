e75f85d5b710d3d0f9a54f5048093c8b9432aba0
Kyung JiHye <k92270217@gmail.com>
2022-11-07 21:23:36 +0900

    del stage

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,6 @@ const SampleData = {
   ],
 };
 
-
 function App() {
   const [data, setData] = useState(SampleData);
 
@@ -36,29 +35,17 @@ function App() {
       <header>
         <Nav />
       </header>
+
       <main className='justify - start px - 6 container mx-auto mt-5 flex max-w-full space-y-0 overflow-scroll'>
         {data.books.map((book) => (
           <Book key={book.id} book={book} />
         ))}
         <div>
           <button className='md: - 0 shrink rounded-sm bg-emerald-500 px-5 py-1 font-medium text-white hover:bg-emerald-700'>
-            {' '}
-            + Add book{' '}
+            + Add book
           </button>
         </div>
 
-        <div className='justify - start px - 6 container mx-auto mt-5 flex max-w-full space-y-0 overflow-scroll'>
-          {data.books.map((book) => (
-            <Book key={book.id} book={book} />
-          ))}
-        </div>
-        <div>
-          <button className='md: - 0 shrink rounded-sm bg-emerald-500 px-5 py-1 font-medium text-white hover:bg-emerald-700'>
-            {' '}
-            + Add book{' '}
-          </button>
-        </div>
-        ;
       </main>
       {/* <footer>
         Copyright Indiflex Jeje Cording
